4af175b33693f8f5b004ed78d8f1ea06a0e2cbda
kclown0 <keumdy0@gmail.com>
2022-09-19 10:20:16 +0900

    start

---
+++ server/seb39_main_project002/src/main/java/mainproject/Seb39MainProject002Application.java
@@ -0,0 +1,13 @@
+package mainproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb39MainProject002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb39MainProject002Application.class, args);
+	}
+
+}
---
+++ server/seb39_main_project002/src/test/java/mainproject/Seb39MainProject002ApplicationTests.java
@@ -0,0 +1,13 @@
+package mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb39MainProject002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
