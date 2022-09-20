d30bbf9f7f74e2102ff5567a93b2b707753f0692
RyuJiChang <rgc0582@gmail.com>
2022-09-20 12:10:51 +0900

    signup,login axios

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 // import { Link } from "react-router-dom";
 
 function Login() {
@@ -15,22 +16,21 @@ function Login() {
     window.location.href = `http://localhost:3000/login`;
   }
   function postForm(username, password) {
-    fetch("http://15.164.53.160:8080/v1/members/join", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
+    axios
+      .post("http://15.164.53.160:8080/v1/members/join", {
         username,
         password,
-        keeplogin: isChecked,
-      }),
-    }).then((res) => {
-      if (res.status === 201) {
-        linkToLogin();
-      }
-    });
+      })
+      .then(function (response) {
+        console.log(response);
+        if (response.status === 201) {
+          linkToLogin();
+        }
+      })
+      .catch(function (error) {
+        // linkToLogin(); //에러로 인해 이동 되는지 테스트 하는 용도
+        console.log(error);
+      });
   }
 
   const handleSubmit = (event) => {
@@ -52,9 +52,6 @@ function Login() {
       alert(Error.join("\n\n"));
     }
     if (!Error.length) {
-      alert(
-        `congratulation!  ${memberId}\nNow you can Log in to stackoverflow`
-      );
       //   linkToLogin(); // 아래 작업이 되어야 하지만 일단 post가 안되는 상황이라 로그인 이동만 체크
       postForm(memberId, memberPassword);
     }
